a841be20ec2c1d1ce78ec8712c61fe16a0663cb6
Veronika <v.shishkowa@yandex.ru>
2022-08-31 13:22:12 +0300

    Добавила папку Seminar3

---
+++ Seminar3/Program.cs
@@ -0,0 +1,56 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+// void Square(int n)
+// {
+//     int curr = 1;
+//     int result = 0;
+//     while (curr <= n)
+//     {
+//         result = curr * curr * curr;
+
+//         if (curr == n)
+//         {
+//             Console.Write(result);
+//         }
+//         else
+//         {
+
+//             Console.Write(result + ",");
+//         }
+//         curr++;
+//     }
+// }
+// Console.Write("Введите цифру: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// Square(num);
+
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+// Console.Write("Введите число: ");
+// string? number = Console.ReadLine();
+
+// void FiveNumber(string number)
+// {
+//     if (number[0] == number[4] || number[1] == number[3])
+//     {
+//         Console.WriteLine("Ваше число: {number} - палиндром.");
+//     }
+//     else Console.WriteLine("Ваше число: {number} - НЕ палиндром.");
+// }
+
+// if (number!.Length == 5)
+// {
+//     FiveNumber(number);
+// }
+// else Console.WriteLine("Введи правильное число");
+
+
+
+
+
+
+
+ 
